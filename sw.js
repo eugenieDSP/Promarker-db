ecb35a97c28fff9cf18b0fc94b3f1bc6bd3688b1
Eugenie de Saint-Phalle <8045474+eugenieDSP@users.noreply.github.com>
2026-07-10 08:44:24 +0200

    Add files via upload
    Minor changes to the UI and content

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "promarker-studio-personal-v5";
+const CACHE = "promarker-studio-personal-v9";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 
 self.addEventListener("install", (e) => {

1c28d8cd22e8b8da6788e1ec934a882cd1de3861
Eugenie de Saint-Phalle <8045474+eugenieDSP@users.noreply.github.com>
2026-07-11 10:36:41 +0200

    Add files via upload
    Fixing tokens

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "promarker-studio-personal-v11";
+const CACHE = "promarker-studio-personal-v12";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 
 self.addEventListener("install", (e) => {
